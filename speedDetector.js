917c4df043892bcf1df9d9cb0e3f75fa398f9d9d
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-11-26 20:44:32 +0300

    speed detector and student grading challenges

---
+++ speedDetector.js
@@ -0,0 +1,17 @@
+function speedDetector(){
+    let speed= prompt('What was the speed(km/s) of the car?')
+    
+    let dpoints= (speed>70)? (speed-70)/5 : 'Ok';
+    
+    if (dpoints==='Ok'){
+        return 'Ok';
+    }
+    else if (dpoints>=13){
+        return 'Licence suspended!'
+    }
+    else if (dpoints<13){
+    
+        return Math.floor (dpoints);
+    } 
+    }
+    console.log(speedDetector());
---
+++ studentGrading.js
@@ -0,0 +1,20 @@
+function grading() {
+    let marks = prompt("Enter Student's Marks:" )
+    if(marks>=80){
+        return 'Grade: A';
+    }
+    else if (marks>=60 && marks<80){
+        return 'Grade: B';
+    }
+    else if (marks>=50 && marks<60){
+        return 'Grade: C';
+    }
+    else if (marks>=40 && marks<50){
+        return 'Grade: D';
+    }
+    else if (marks<40){
+        return 'Grade: E';
+    }
+    }
+    console.log(grading ());
+    

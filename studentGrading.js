2f5d4ace098c983867365daceb3ab1b869dc63b7
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-11-28 12:27:46 +0300

    added user input validation

--- studentGrading.js
+++ studentGrading.js
@@ -1,8 +1,20 @@
 const prompt = require('prompt-sync')();
-//Function for grading marks from the user input
+let marks = parseInt(prompt("Enter Student's Marks:" )) //prompt the user for marks
+const grade=grading();
+
+if(marks < 0 || marks > 100){ //checks the value entered by the use if is a number between >=0 and <=100
+    console.log('Please enter marks in the range (0 to 100)')
+    return;
+}
+else if (isNaN(marks)){
+    console.log('Please enter marks in the range (0-100)')
+    return;
+}else 
+return console.log(grade);
+// //Function for grading marks from the user input
 function grading() {
-    let marks = parseInt(prompt("Enter Student's Marks:" )) //prompt the user for marks
-    if(marks>=80){
+    
+    if(marks>=80){ 
         return 'Grade: A';
     }
     else if (marks>=60 && marks<80){
@@ -18,5 +30,5 @@ function grading() {
         return 'Grade: E';
     }
     }
-    console.log(grading ());
+ 
     
